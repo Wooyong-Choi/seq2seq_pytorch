fix softmax dim

seq2seq/model/Attn.py
@@ -34,8 +34,8 @@ class Attn(nn.Module):
         if self.gpu_id != -1:
             attn_weights = attn_weights.cuda(self.gpu_id)
 
-        attn_weights = F.softmax(attn_weights)
-        
+        attn_weights = F.softmax(attn_weights, dim=1)
+
         context = torch.bmm(attn_weights, encoder_outputs)
         
         # Attentional vector using the RNN hidden state and context vector

seq2seq/model/DecoderRNN.py
@@ -43,7 +43,7 @@ class DecoderRNN(nn.Module):
 
         # Finally predict next token (Luong eq. 6, without softmax)
         outputs = self.out(outputs.contiguous().view(-1, self.hidden_size))
-        outputs = self.softmax(outputs).view(cur_batch_size, max_len, -1)
+        outputs = self.softmax(outputs, dim=1).view(cur_batch_size, max_len, -1)
         
         # Return final output, hidden state, and attention weights (for visualization)
         return outputs, hidden, attn_weights